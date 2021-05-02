ddb29c9ddffc76545357653b57d3ed0d24caa49f
matan <matanmula@mail.tau.ac.il>
2021-05-02 14:07:57 +0300

    added square root button and ANS square button

--- calculator.py
+++ calculator.py
@@ -42,6 +42,10 @@ class Calculator:
         self.ln_ans_button = Button(master, text="ln(ANS)", command=lambda: self.set_total_label("log"))
         # Clicking this button will set total_label_text to e^(total_label_text)
         self.e_power_ans_button = Button(master, text="e^(ANS)", command=lambda: self.set_total_label("e_power"))
+        # Clicking this button will set total_label_text to sqrt(total_label_text)
+        self.sqrt_ans_button = Button(master, text="ANS^0.5", command=lambda: self.set_total_label("sqrt"))
+        # Clicking this button will set total_label_text to total_label_text^(2)
+        self.squared_ans_button = Button(master, text="ANS^2", command=lambda: self.set_total_label("squared"))
 
         # All these buttons add the matching action to the input entry
         self.plus_button = Button(master, text="+", command=lambda: self.add_action_to_entry("+"))
@@ -52,15 +56,17 @@ class Calculator:
         # LAYOUT
 
         self.label.grid(row=0, column=0, sticky=W)
-        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
-        self.ans_button.grid(row=0, column=3)
+        self.total_label.grid(row=0, column=1, columnspan=1, sticky=E)
+        self.ans_button.grid(row=0, column=2)
+        self.reset_button.grid(row=0, column=3, sticky=W + E)
 
         self.entry.grid(row=1, column=0, columnspan=3, sticky=W + E)
-        self.reset_button.grid(row=1, column=3, sticky=W + E)
+        self.equal_button.grid(row=1, column=3)
 
-        self.equal_button.grid(row=2, column=3)
-        self.ln_ans_button.grid(row=2, column=2)
-        self.e_power_ans_button.grid(row=2, column=1)
+        self.sqrt_ans_button.grid(row=2, column=0)
+        self.squared_ans_button.grid(row=2, column=1)
+        self.ln_ans_button.grid(row=2, column=3)
+        self.e_power_ans_button.grid(row=2, column=2)
 
         self.plus_button.grid(row=3, column=0)
         self.subtract_button.grid(row=3, column=1)
@@ -79,33 +85,43 @@ class Calculator:
                 t = expressionparse.Tree()
                 t.parse(expression)
                 self.total = t.evaluate()
-                self.total_label_text.set(str(self.total))
             except:
-                self.total_label_text.set("Incorrect math expression")
+                self.total = "Incorrect math expression"
         elif method == 'log':
             if self.total_label_text.get() != "Incorrect math expression" and float(self.total_label_text.get()) > 0:
                 self.total = math.log(float(self.total_label_text.get()))
-                self.total_label_text.set(str(self.total))
             else:
-                self.total_label_text.set("Incorrect math expression")
+                self.total = "Incorrect math expression"
         elif method == 'e_power':
             if self.total_label_text.get() != "Incorrect math expression":
                 self.total = math.exp(float(self.total_label_text.get()))
-                self.total_label_text.set(str(self.total))
+        elif method == 'squared':
+            if self.total_label_text.get() != "Incorrect math expression":
+                self.total = math.pow(float(self.total_label_text.get()), 2)
+        elif method == 'sqrt':
+            if self.total_label_text.get() != "Incorrect math expression" and float(self.total_label_text.get()) >= 0:
+                self.total = math.sqrt(float(self.total_label_text.get()))
+            else:
+                self.total = "Incorrect math expression"
         elif method == 'reset':
-            self.total_label_text.set('0')
+            self.total = '0'
 
+        self.total_label_text.set(str(self.total))
         self.entry_text.set('0')
+        self.entry.focus()
+        self.entry.icursor(END)
 
     # This function adds the content in total_label_text to the input entry
     def write_answer_to_entry(self):
         if self.total_label_text.get() != "Incorrect math expression":
             self.entry_text.set(self.entry_text.get()+self.total_label_text.get())
+            self.entry.focus()
             self.entry.icursor(END)
 
     # This function adds the matching action to the input entry
     def add_action_to_entry(self, action):
         self.entry_text.set(self.entry_text.get() + action)
+        self.entry.focus()
         self.entry.icursor(END)
 
 
